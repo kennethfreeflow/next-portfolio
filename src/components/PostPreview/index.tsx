eff4326807421dd7ea8675baeb83446a5b65f3ee
Kenneth Ng <76455368+kennethfreeflow@users.noreply.github.com>
2022-04-19 21:05:27 -0700

    Update PostPreview

---
+++ src/components/PostPreview/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import CoverImage from "../CoverImage";
+
+export default function PostPreview({ title, coverImage, excerpt, slug }) {
+  return (
+    <div>
+      <div className="mb-5">
+        <CoverImage
+          slug={slug}
+          title={title}
+          src={coverImage}
+          height={278}
+          width={556}
+        />
+      </div>
+      <h3 className="text-3xl mb-3 leading-snug">
+        <Link href={`/posts/${slug}`}>
+          <a className="hover:underline">{title}</a>
+        </Link>
+      </h3>
+      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+    </div>
+  );
+}
