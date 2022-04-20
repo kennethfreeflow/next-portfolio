209fb8f627453925afc2d76ed47014237d979d1b
Kenneth Ng <76455368+kennethfreeflow@users.noreply.github.com>
2022-04-19 21:05:38 -0700

    Update NavBar with menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,47 +1,77 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { SVGProps } from 'react'
-import { TerminalIcon } from '@heroicons/react/outline'
-import logo from '../../../public/workflow-mark-indigo-600.svg'
-
-type INavItem = {
-  label: string;
-  href: string;
-  icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
-}
-
-const navItems: INavItem[] = [
-  {
-    label: 'Projects',
-    icon: TerminalIcon,
-    href: '/projects',
-  }
-]
+import { Disclosure, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export default function Navbar() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6">
-      <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
-          <a href="#">
-            <span className="sr-only">Workflow</span>
-            <Image
-              height={50}
-              width={50}
-              src={logo}
-              alt=""
-            />
-          </a>
-        </div>
-        {navItems.map((navItem) => (
-          <Link key={navItem.label} href="/projects">
-            <a className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <navItem.icon className="flex-shrink-0 h-6 w-6 text-primary-500" aria-hidden="true" />
-              <span className="ml-2 text-base font-medium text-gray-900">{navItem.label}</span>
-            </a>
-          </Link>
-        ))}
-      </div>
-    </div>
-  )
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="block lg:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  />
+                </div>
+              </div>
+              <div className="-mr-2 flex">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+            </div>
+          </div>
+
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel>
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Blog
+                </Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
+  );
 }
