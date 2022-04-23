15aed74982d01875ecf08b4be542ef0bab77cf8a
kennethlng <76455368+kennethfreeflow@users.noreply.github.com>
2022-04-23 15:05:07 -0700

    Create frontMatter constants

---
+++ src/constants/frontMatter.ts
@@ -0,0 +1,7 @@
+export const TITLE = 'title';
+export const DESCRIPTION = 'description';
+export const DATE = 'date';
+export const FEATURED_IMAGE = 'featured_image';
+export const SLUG = 'slug';
+export const TAGS = 'tags';
+export const CONTENT = 'content';
